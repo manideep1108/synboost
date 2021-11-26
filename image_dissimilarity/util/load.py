382496760419fb21975192370f1ca5ff8de8c854
Shanmukh-C <85222764+Shanmukh-C@users.noreply.github.com>
2021-11-27 01:58:02 +0530

    Update

--- image_dissimilarity/util/load.py
+++ image_dissimilarity/util/load.py
@@ -8,7 +8,7 @@ import os
 def load_ckp(wandb_base_path, name, epoch):      
 
     # load check point
-    path = name + "/" + name + "_" + str(epoch) + ".pth"
+    path = "results/"+name + "/" + name + "_" + str(epoch) + ".pth"
     load_path = os.path.join(wandb_base_path, path)
     wandb.restore(path)
     checkpoint = torch.load(load_path)
