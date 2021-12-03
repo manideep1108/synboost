7bf744dbe7ba62164797d9374b426802fe4f8856
Shanmukh-C <85222764+Shanmukh-C@users.noreply.github.com>
2021-12-03 22:44:00 +0530

    Update

--- image_dissimilarity/trainers/dissimilarity_trainer_lightning.py
+++ image_dissimilarity/trainers/dissimilarity_trainer_lightning.py
@@ -73,10 +73,17 @@ class Synboost_trainer(pl.LightningModule):
         # print(self.data_module)
         # print(len(DataLoader(self.test_dataset1, **self.config["test_dataloader1"]['dataloader_args']))) #for debugging
         # print(self.data_module.val_dataloader())  #just for debugging
-
+        
+        print("****************************************")
+        self.val_size = len(self.data_module.val_dataloader()[0])
+        print(self.val_size)       
         self.test_loader1_size = len(self.data_module.val_dataloader()[1])
+        print(self.test_loader1_size)
         self.test_loader2_size = len(self.data_module.val_dataloader()[2])
+        print(self.test_loader2_size)
         self.test_loader3_size = len(self.data_module.val_dataloader()[3])
+        print(self.test_loader3_size)
+        print("****************************************")
         #self.test_loader4_size = len(self.datamodule.test_dataloader()[3])
         
         self.flat_pred = [torch.zeros(h*w*self.test_loader1_size).cuda(),torch.zeros(h*w*self.test_loader2_size).cuda(),torch.zeros(h*w*self.test_loader3_size).cuda()]
