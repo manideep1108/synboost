87691c95726f01b2405dfe091d33600f221525b5
Shanmukh-C <85222764+Shanmukh-C@users.noreply.github.com>
2021-12-10 00:24:21 +0530

    Update

--- image_dissimilarity/train_lightning.py
+++ image_dissimilarity/train_lightning.py
@@ -65,10 +65,11 @@ wandb_logger = WandbLogger(project='MLRC_Synboost', # group runs in "BANA" proje
 
 
 checkpoint_callback = ModelCheckpoint(
+    monitor='val_loss/dataloader_idx_0',
     # dirpath=f"{cfg.NAME}",    
     # filename='{epoch}-{train_loss:.2f}',   # right now checking based on train_loss
     save_top_k =1,                 # saving best model, if to save the latest one replace by - save_last=True
-    #mode='min',                     # written for save_top_k
+    mode='min',                     # written for save_top_k
     every_n_epochs=1,              # after 40 epochs checkpoint saved.
     save_on_train_epoch_end=True   #  to run checkpointing at the end of the training epoch.  
     )
