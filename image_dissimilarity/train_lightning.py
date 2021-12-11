b2788c585de4a50ea4e7769ec7ba90b4540e646e
Shanmukh-C <85222764+Shanmukh-C@users.noreply.github.com>
2021-12-11 13:14:49 +0530

    Update

--- image_dissimilarity/train_lightning.py
+++ image_dissimilarity/train_lightning.py
@@ -109,7 +109,7 @@ if opts.wandb_resume:
     run = wandb.init()  
     artifact = run.use_artifact(opts.artifact_path, type='model')
     artifact_dir = artifact.download()  #should change these lines so that user can specify path (now just for testing)
-    model = Synboost_trainer.load_from_checkpoint(Path(artifact_dir)/'model.ckpt')
+    model = Synboost_trainer(config).load_from_checkpoint(Path(artifact_dir)/'model.ckpt')
 
 
 trainer = Trainer(max_epochs=6, gpus=1, log_every_n_steps=1, logger=wandb_logger,  callbacks=[checkpoint_callback, checkpoint_callback_latest])
