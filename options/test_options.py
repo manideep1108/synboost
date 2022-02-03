b98ba8612944569a9d915ed5cdc2d5539ff598e7
ShashwatNaidu <85156786+ShashwatNaidu@users.noreply.github.com>
2022-02-03 10:00:47 +0530

    Update test_options.py

--- options/test_options.py
+++ options/test_options.py
@@ -4,17 +4,18 @@ from .base_options import BaseOptions
 class TestOptions(BaseOptions):
     def initialize(self, parser):
         BaseOptions.initialize(self, parser)
-        parser.add_argument('--results_dir', type=str, default='./results/', help='saves results here.')
+        parser.add_argument('--results_dir', type=str, default='/kaggle/working/results/', help='saves results here.')
         parser.add_argument('--which_epoch', type=str, default='latest', help='which epoch to load? set to latest to use latest cached model')
         parser.add_argument('--how_many', type=int, default=float("inf"), help='how many test images to run')
-
+        parser.add_argument('--instances_og', type=str, default='/kaggle/input/instance', help='path to folder containing ground truth instances')
+        parser.add_argument('--semantic_og', type=str, default='/kaggle/input/semantic', help='path to folder containing ground truth semantic maps')
         # General
-        parser.add_argument('--demo-folder', type=str, default='./sample_images', help='path to the folder containing demo images',
+        parser.add_argument('--demo-folder', type=str, default='/kaggle/input/original', help='path to the folder containing demo images/ original images',
                             required=False)
         parser.add_argument('--no-segmentation', action='store_true', help='if specified, do *not* segment images since they are already created')
 
         # Segmentation
-        parser.add_argument('--snapshot', type=str, default='models/image-segmentation/cityscapes_best.pth',
+        parser.add_argument('--snapshot', type=str, default='/kaggle/input/sdcnet-cityscapes-segmentation/cityscapes_best (1).pth',
                             help='pre-trained Segmentation checkpoint', required=False)
         parser.add_argument('--arch', type=str, default='network.deepv3.DeepWV3Plus',
                             help='Network architecture used for Segmentation inference')
