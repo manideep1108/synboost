f71eb501b06d7c911d780297ac7c3082cf1899c1
Manideep Sridhara <73156028+manideep1108@users.noreply.github.com>
2022-02-05 08:22:25 +0530

    update

--- test.bash
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-CUDA_VISIBLE_DEVICES=0 python test.py --config configs/test/road_anomaly_configuration.yaml --gpu_ids 0
-CUDA_VISIBLE_DEVICES=0 python test.py --config configs/test/fs_static_configuration.yaml --gpu_ids 0
-CUDA_VISIBLE_DEVICES=0 python test.py --config configs/test/fs_lost_found_configuration.yaml --gpu_ids 0
